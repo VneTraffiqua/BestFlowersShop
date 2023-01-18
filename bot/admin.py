f4398b5d55f93244e5c239150245e8a57554925c
VneTraffiqua <safe72again@gmail.com>
2023-01-18 07:02:23 -0800

    Update admin.py

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Customer, Bouquet, Order, Category
 
-# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Bouquet)
+admin.site.register(Order)
+admin.site.register(Category)
+
+# @admin.register(Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'name', 'delivery_date', 'delivery_time'
+#     )
+#
+#
+# @admin.register(Bouquet)
+# class BouquetAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'title', 'price'
+#     )
