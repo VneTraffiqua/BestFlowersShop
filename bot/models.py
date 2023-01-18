428a83aa51e6d0dbce7562c66125cf4adcec5fd7
VneTraffiqua <safe72again@gmail.com>
2023-01-18 07:02:15 -0800

    Update models

--- bot/models.py
+++ bot/models.py
@@ -4,10 +4,38 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+class Category(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Категория',
+        help_text='Введите категорию'
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
 class Bouquet(models.Model):
-    title = models.CharField('Название', max_length=255)
-    image = models.ImageField('Изображение')
-    price = models.IntegerField('Стоимость')
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=255,
+        help_text='Введите название'
+    )
+    category = models.ForeignKey(
+        to=Category, on_delete=models.CASCADE, verbose_name='Категория',
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='/media',
+        
+    )
+    price = models.IntegerField(
+        'Стоимость'
+    )
     bouquet_meaning = models.CharField('Описание', max_length=255)
     status = models.BooleanField()
 
