0aab2590e8543f1c309c6548ef31bdae4ea3dee7
Elnar Mengelbaev <elnar_men@mail.ru>
2023-01-19 19:23:41 +0300

    доработал меню бота

--- bot/models.py
+++ bot/models.py
@@ -1,21 +1,59 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Customer(models.Model):
-    id = models.IntegerField()
-    name = models.CharField('Имя', max_length=255)
-    address = models.CharField('Адрес', max_length=255)
-    delivery_date = models.DateField('Дата доставки', )
-    delivery_time = models.TimeField('Время доставки', )
-    bouquet = models.ManyToManyField('Букет', related_name='costomers')
+class Category(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Категория',
+        help_text='Введите категорию'
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Bouquet(models.Model):
-    title = models.CharField('Название', max_length=255)
-    image = models.ImageField('Изображение')
-    price = models.IntegerField('Стоимость')
-    bouquet_meaning = models.CharField('Смысл букета', max_length=255)
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=255,
+        help_text='Введите название'
+    )
+    category = models.ForeignKey(
+        to=Category, on_delete=models.CASCADE, verbose_name='Категория',
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='media',
+
+    )
+    price = models.IntegerField(
+        'Стоимость'
+    )
+    bouquet_meaning = models.CharField('Описание', max_length=255)
+
+    floral_composition = models.CharField('Цветочная композиция', max_length=255)
+
+    status = models.BooleanField()
+
+
+class Order(models.Model):
+    address = models.CharField('Адрес', max_length=255)
+    delivery_date = models.DateField('Дата доставки', )
+    delivery_time = models.TimeField('Время доставки', )
+    bouquet = models.ManyToManyField(
+        'Bouquet', blank=True, related_name='bouquets'
+    )
+
+
+class Customer(models.Model):
+    name = models.CharField('Имя', max_length=255)
     phone_number = PhoneNumberField(
         region='RU',
         blank=True,
@@ -23,6 +61,11 @@ class Bouquet(models.Model):
         verbose_name='Номер покупателя',
         db_index=True
     )
-    floral_composition = models.CharField('Цветочная композиция', max_length=255)
-    status = models.BooleanField()
-
+    orders = models.ForeignKey(
+        to=Order,
+        on_delete=models.CASCADE,
+        verbose_name='Заказы',
+        related_name='Orders',
+        null=True,
+        blank=True
+    )
--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flower_shop_bot.settings")
 
 import django
-
+import phonenumbers
 django.setup()
 
 from dotenv import load_dotenv
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, LabeledPrice, Bot
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           CallbackContext, ConversationHandler, PreCheckoutQueryHandler)
-from bot.models import Customer, Bouquet
 from service_functions import *
+from bot.models import Customer, Bouquet, Order
 
-BOUQUET_OCCASION, PRICE, ORDER, ADDRESS,  = range(5)
+
+BOUQUET_OCCASION, PRICE, FORK, ADDRESS, DELIVERY_TIME, \
+    PHONE, PHONE_VALIDATOR, FLORIST, VIEWING_COLLECTION, START_OR_STOP = range(10)
 
 load_dotenv()
 
@@ -36,7 +38,8 @@ def start(update: Update, context: CallbackContext):
         resize_keyboard=True,
         one_time_keyboard=True
     )
-    context.user_data['choice'] = 'Событие'
+
+    context.user_data['choice'] = 'event'
 
     update.message.reply_text(
         'К какому событию готовимся? Выберите один из вариантов, либо укажите свой.',
@@ -46,33 +49,13 @@ def start(update: Update, context: CallbackContext):
     return BOUQUET_OCCASION
 
 
-# def choose_other_occasion(update: Update, context: CallbackContext):
-#
-#     user_data = context.user_data
-#     text = update.message.text
-#     category = user_data['choice']
-#     user_data[category] = text
-#     del user_data['choice']
-#
-#
-#     update.message.reply_text(
-#         'Напишите повод'
-#     )
-#
-#     return PRICE
-
-
 def choose_price(update: Update, context: CallbackContext):
 
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
+    save_user_choice(update, context)
 
     message_keyboard = [
-        ['~500', '~1000'],
-        ['~2000', 'Больше'],
+        ['500', '1000'],
+        ['2000', 'Больше'],
         ['Не важно']
     ]
 
@@ -92,15 +75,9 @@ def choose_price(update: Update, context: CallbackContext):
     return PRICE
 
 
-
-
 def send_bouquet_information(update: Update, context: CallbackContext):
 
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
+    save_user_choice(update, context)
 
     message_keyboard = [
         ['Заказать букет'],
@@ -113,21 +90,53 @@ def send_bouquet_information(update: Update, context: CallbackContext):
         one_time_keyboard=True
     )
 
-    photo = open('bot/media/boquet.jpg', "rb")
+    event = context.user_data['event']
 
-    floral_composition = 'Роза'
-    price = 1000
+    if context.user_data['price'] == 'Больше':
+        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event,
+                                                  price__gt=2000)
+
+    elif context.user_data['price'] == '500':
+        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event,
+                                                  price__lt=500)
+
+    elif context.user_data['price'] == '1000':
+        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event,
+                                                  price__range=(500, 1000))
+
+    elif context.user_data['price'] == '2000':
+        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event,
+                                                  price__range=(1000, 2000))
+
+    else:
+        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event)
+
+    context.user_data['filtered_bouquets_collection'] = filtered_bouquets_collection
+
+    context.user_data['bouquet_index'] = 0
+
+    bouquet = filtered_bouquets_collection[context.user_data['bouquet_index']]
+
+    context.user_data['selected_bouquet'] = bouquet
+
+    photo = bouquet.image
+
+    floral_composition = bouquet.floral_composition
+
+    price = bouquet.price
 
     text = f'''
-    Этот букет несет в себе всю нежность ваших чувств и не способен оставить равнодушных ни одного сердца!
+    {bouquet.title}
     
-    Цветочная композиция: {floral_composition}
+{bouquet.bouquet_meaning}
     
-    Стоимость: {price}
-    
-    Хотите что-то еще более уникальное? Подберите другой букет из нашей коллекции или закажите консультацию флориста
+Цветочная композиция: {floral_composition}
 
-     '''
+Стоимость: {price}
+
+Хотите что-то еще более уникальное? Подберите другой букет из нашей коллекции или закажите консультацию флориста
+    '''
+
     bot = Bot(token=TELEGRAM_TOKEN)
 
     bot.send_photo(
@@ -137,7 +146,7 @@ def send_bouquet_information(update: Update, context: CallbackContext):
         reply_markup=markup
     )
 
-    return ORDER
+    return FORK
 
 
 def get_customer_name(update: Update, context: CallbackContext):
@@ -153,24 +162,212 @@ def get_customer_name(update: Update, context: CallbackContext):
 
 def get_address(update: Update, context: CallbackContext):
 
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
+    save_user_choice(update, context)
 
     context.user_data['choice'] = 'address'
 
+    update.message.reply_text(
+        'Введите адрес доставки'
+    )
 
+    return DELIVERY_TIME
+
+
+def get_delivery_time(update: Update, context: CallbackContext):
+
+    save_user_choice(update, context)
+
+    context.user_data['choice'] = 'delivery_time'
+
+    update.message.reply_text(
+        'Укажите дату и время доставки'
+    )
+
+    return PHONE
+
+
+def get_phonenumber(update: Update, context: CallbackContext):
+
+    save_user_choice(update, context)
+
+    context.user_data['choice'] = 'phonenumber'
+
+    update.message.reply_text(
+        'Введите номер телефона'
+    )
+
+    return PHONE_VALIDATOR
+
+
+def validate_phonenumber(update: Update, context: CallbackContext):
+
+    phonenumber = update.message.text
+
+    try:
+        pure_phonenumber = phonenumbers.parse(
+            phonenumber, 'RU'
+        )
+    except phonenumbers.phonenumberutil.NumberParseException:
+        return enter_number_again(update, context)
+
+    while not phonenumbers.is_valid_number(pure_phonenumber):
+        return enter_number_again(update, context)
+
+    if context.user_data.get('name'): # если ввод номера произошел при заказе
+
+        save_user_choice(update, context)
+
+        return save_models(update, context)
+
+    return send_information_to_florist(update, context)
+
+
+def enter_number_again(update: Update, context: CallbackContext):
+
+    update.message.reply_text(
+        'Введите валидный номер телефона'
+    )
+
+    return PHONE_VALIDATOR
+
+
+def save_models(update: Update, context: CallbackContext):
+
+    name = context.user_data['name']
+    phonenumber = context.user_data['phonenumber']
+
+    address = context.user_data['address']
+    delivery_date = context.user_data['delivery_time'].split()[0]
+    delivery_time = context.user_data['delivery_time'].split()[1]
+    bouquet = context.user_data['selected_bouquet']
+
+    order = Order.objects.create(
+        address=address,
+        delivery_date=delivery_date,
+        delivery_time=delivery_time,
+    )
+
+    order.bouquet.set([bouquet])
+    order.save()
+
+    customer = Customer.objects.get_or_create(name=name, phone_number=phonenumber)[0]
+
+    customer.ordedrs = order
+    customer.save()
+
+    return send_information_to_courier(update, context)
 
 
 def order_consultation(update: Update, context: CallbackContext):
-    pass
 
+    update.message.reply_text(
+        'Укажите номер телефона, и наш флорист перезвонит вам в течение 20 минут'
+    )
+
+    return PHONE_VALIDATOR
+
+
+def send_information_to_florist(update: Update, context: CallbackContext):
+
+    context.user_data['choice'] = 'phone_number_to_florist'
+    save_user_choice(update, context)
+
+
+    update.message.reply_text(
+        'Флорист скоро свяжется с вами. А пока можете присмотреть что-нибудь из готовой коллекции'
+    )
+
+    #Флорист с фиксированным ID получает контакты клиента, кому звонить.
+
+    return view_full_collection(update, context)
+
+
+def send_information_to_courier(update: Update, context: CallbackContext):
+
+    message_keyboard = [['Стоп', 'Заново']]
+
+    markup = ReplyKeyboardMarkup(
+        message_keyboard,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+    update.message.reply_text(
+        text='''Ваш заказ принят, ожидайте доставки
+
+Хотите начать заново?''',
+        reply_markup=markup
+    )
+
+    return START_OR_STOP
 
 def view_full_collection(update: Update, context: CallbackContext):
-    pass
 
+    filtered_bouquets_collection = context.user_data['filtered_bouquets_collection']
+    if 'bouquet_index' in context.user_data:
+        context.user_data['bouquet_index'] += 1
+
+    if context.user_data['bouquet_index'] >= len(filtered_bouquets_collection):
+        context.user_data['bouquet_index'] = 0
+
+    index = context.user_data['bouquet_index']
+
+    bouquet = filtered_bouquets_collection[index]
+    context.user_data['selected_bouquet'] = bouquet
+
+    photo = bouquet.image
+    floral_composition = bouquet.floral_composition
+    price = bouquet.price
+    text = f'''
+    {bouquet.title}
+    
+{bouquet.bouquet_meaning}
+        
+Цветочная композиция: {floral_composition}
+
+Стоимость: {price}
+        '''
+
+    message_keyboard = [
+        ['Заказать букет', 'Другой букет']
+    ]
+
+    markup = ReplyKeyboardMarkup(
+        message_keyboard,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+
+    bot = Bot(token=TELEGRAM_TOKEN)
+
+    try:
+        bot.send_photo(
+            chat_id=update.message.chat_id,
+            photo=photo,
+            caption=text,
+            reply_markup=markup
+        )
+
+        return VIEWING_COLLECTION
+
+    except:
+
+        message_keyboard = [
+            ['Стоп', 'Заново'],
+        ]
+
+        markup = ReplyKeyboardMarkup(
+            message_keyboard,
+            resize_keyboard=True,
+            one_time_keyboard=True
+        )
+        update.message.reply_text(
+            text='Вы просмотрели всю коллекцию, хотите начать заново?',
+            reply_markup=markup
+        )
+
+        return START_OR_STOP
 
 def main():
     load_dotenv()
@@ -181,6 +378,7 @@ def main():
         entry_points=[CommandHandler('start', start)],
         allow_reentry=True,
         states={
+
             BOUQUET_OCCASION: [
                 MessageHandler(
                     Filters.regex('^(День рождения|Свадьба|Без повода)$'),
@@ -192,15 +390,34 @@ def main():
             ],
 
             PRICE: [MessageHandler(Filters.text, send_bouquet_information)],
-            ORDER: [
+
+            FORK: [
                 MessageHandler(Filters.regex('^Заказать букет'), get_customer_name),
                 MessageHandler(Filters.regex('^Заказать консультацию'), order_consultation),
                 MessageHandler(Filters.regex('^Посмотреть всю коллекцию'), view_full_collection),
             ],
+
             ADDRESS: [MessageHandler(Filters.text, get_address)],
 
+            DELIVERY_TIME: [MessageHandler(Filters.text, get_delivery_time)],
+
+            PHONE: [MessageHandler(Filters.text, get_phonenumber)],
+
+            PHONE_VALIDATOR: [MessageHandler(Filters.text, validate_phonenumber)],
+
+            FLORIST: [MessageHandler(Filters.text, send_information_to_florist)],
+
+            VIEWING_COLLECTION: [
+                MessageHandler(Filters.regex('^Другой букет'), view_full_collection),
+                MessageHandler(Filters.regex('^Заказать букет'), get_customer_name),
+            ],
+
+            START_OR_STOP: [
+                MessageHandler(Filters.regex('^Заново'), start),
+            ]
+
         },
-        fallbacks=[MessageHandler(Filters.regex('^Стоп$'), start)],
+        fallbacks=[MessageHandler(Filters.regex('^Стоп$'), end)],
     )
     dispatcher.add_handler(conv_handler)
     dispatcher.add_handler(CommandHandler("start", start))
