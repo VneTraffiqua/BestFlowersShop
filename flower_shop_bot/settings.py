2211c461b6e29f560a5cf80a1d4791c3d6d2c8c9
VneTraffiqua <safe72again@gmail.com>
2023-01-23 01:05:13 -0800

    Update settings.py

--- flower_shop_bot/settings.py
+++ flower_shop_bot/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'flower_shop_bot.wsgi.application'
+WSGI_APPLICATION = 'flower_shop_bot.wsgi.application'
 
 
 # Database
@@ -131,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
