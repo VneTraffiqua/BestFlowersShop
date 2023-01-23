1e5e5e008ed6f4201bdf825b89fa01c4aae9c224
Rinat <swatlprus@gmail.com>
2023-01-23 12:53:20 +0500

    Fix filtres for bouquets

--- main.py
+++ main.py
@@ -106,65 +106,62 @@ def send_bouquet_information(update: Update,
     )
 
     event = context.user_data['event']
+    bouquet_set = Bouquet.objects.all()
 
-    if event not in ('Свадьба', 'День рождения', 'Без повода'):
+    if event not in ('Свадьба', 'День рождения', 'В школу', 'Без повода'):
         context.user_data['event'] = event = 'Без повода'
 
-
-    if context.user_data['price'] == 'Больше':
-        filtered_bouquets_collection = \
-            Bouquet.objects.filter(category__title=event, price__gt=2000)
-
-    elif context.user_data['price'] == '~500':
-        filtered_bouquets_collection = \
-            Bouquet.objects.filter(category__title=event, price__lt=500)
-
-    elif context.user_data['price'] == '~1000':
-        filtered_bouquets_collection = \
-            Bouquet.objects.filter(category__title=event, price__range=(500, 1000))
-
-    elif context.user_data['price'] == '~2000':
-        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event,
-                                                              price__range=(1000, 2000))
-
+    if event != 'Без повода':    
+        if context.user_data['price'] == 'Больше':
+            filtered_bouquets_collection = \
+                bouquet_set.filter(category__title=event, price__gt=2000)
+        elif context.user_data['price'] == '~500':
+            filtered_bouquets_collection = \
+                bouquet_set.filter(category__title=event, price__lt=500)
+        elif context.user_data['price'] == '~1000':
+            filtered_bouquets_collection = \
+                bouquet_set.filter(category__title=event, price__range=(500, 1000))
+        elif context.user_data['price'] == '~2000':
+            filtered_bouquets_collection = \
+                bouquet_set.filter(category__title=event,price__range=(1000, 2000))
+        else:
+            filtered_bouquets_collection = bouquet_set.filter(category__title=event)
     else:
-        filtered_bouquets_collection = Bouquet.objects.filter(category__title=event)
+        if context.user_data['price'] == 'Больше':
+            filtered_bouquets_collection = bouquet_set.filter(price__gt=2000)
+        elif context.user_data['price'] == '~500':
+            filtered_bouquets_collection = bouquet_set.filter(price__lt=500)
+        elif context.user_data['price'] == '~1000':
+            filtered_bouquets_collection = bouquet_set.filter(price__range=(500, 1000))
+        elif context.user_data['price'] == '~2000':
+            filtered_bouquets_collection = bouquet_set.filter(price__range=(1000, 2000))
+        else:
+            filtered_bouquets_collection = bouquet_set
 
     context.user_data['filtered_bouquets_collection'] = filtered_bouquets_collection
-
     context.user_data['bouquet_index'] = 0
-
     bouquet = filtered_bouquets_collection[context.user_data['bouquet_index']]
-
     context.user_data['selected_bouquet'] = bouquet
+    for bouquet in filtered_bouquets_collection[:5]:
+        photo = bouquet.image
+        floral_composition = ', '.join([str(flower) for flower in bouquet.flowers.all()])
+        price = bouquet.price
+        text = f'''
+            Название букета: {bouquet.title}
+            \n Смысл букета: {bouquet.bouquet_meaning}
+            \n Цветочная композиция: {floral_composition}
+            \n Стоимость: {price} рублей
+            \n Хотите что-то еще более уникальное? Подберите другой букет из нашей коллекции или закажите консультацию флориста
+            '''
 
-    photo = bouquet.image
-
-    floral_composition = ', '.join([str(flower) for flower in bouquet.flowers.all()])
-
-    price = bouquet.price
-
-    text = f'''
-    {bouquet.title}
-    
-{bouquet.bouquet_meaning}
-    
-Цветочная композиция: {floral_composition}
-
-Стоимость: {price}
-
-Хотите что-то еще более уникальное? Подберите другой букет из нашей коллекции или закажите консультацию флориста
-    '''
-
-    bot = Bot(token=TELEGRAM_TOKEN)
-
-    bot.send_photo(
-        chat_id=update.message.chat_id,
-        photo=photo,
-        caption=text,
-        reply_markup=markup
-    )
-
+        bot = Bot(token=TELEGRAM_TOKEN)
+        bot.send_photo(
+            chat_id=update.message.chat_id,
+            photo=photo,
+            caption=text,
+            reply_markup=markup
+        )
+        
     return FORK
 
 
